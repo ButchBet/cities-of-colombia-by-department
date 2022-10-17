5e3d9bffe063331f9e7ec9396e0ed8e101dc4c22
Butch Bet <butchbet01@gmail.com>
2022-10-17 15:46:39 -0500

    Created the login to show the department and the cities in case of input and a random department with the respective cities in case of not input.

--- bin/global.js
+++ bin/global.js
@@ -2,6 +2,22 @@
 import minimist from 'minimist';
 import returnDepartments from '../src/departments.js';
 
-const input = minimist(process.argv);
-const departments = returnDepartments();
-console.log(departments['san andres y providencia'])
+const department =  minimist(process.argv)._[2]; // Getting the entered department
+const departments = returnDepartments(); // Getting the object departmentsOfColombia
+const entries = Object.entries(departments);
+
+
+
+if(department) { // check if the input exists
+    if(departments[department.toLowerCase()]) { // check if the department exist
+        const cities = departments[department.toLowerCase()].join(', ');
+        console.log(`${department}: ${cities}.`);
+    } else  {
+        console.log('Please enter an existing department, without accents and thus.')
+    }
+} else { // Show a random repartent in case of not input
+    const randomIndex = Math.floor(Math.random() * entries.length);
+    const randomDep = entries[randomIndex][0];
+    const randomCities = entries[randomIndex][1].join(', ');
+    console.log(`${randomDep}: ${randomCities}.`);
+}
